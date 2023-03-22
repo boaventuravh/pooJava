284ebc3638c7b200c5e15c5a40fb256404e08dfd
aluno.ssa <aluno.ssa@SSA-D-INF02-03.intranet.cefetba.br>
2023-03-22 19:07:27 -0300

    Código do professor

--- 20230321EstrururasDinamicas/estruturasdinamicas/Principal.java
+++ 20230321EstrururasDinamicas/estruturasdinamicas/Principal.java
@@ -5,23 +5,46 @@ import java.util.HashMap;
 
 public class Principal {
 
-	public static void main(String[] args) {
+ public static void main(String[] args) {
+  String lista[]= {"Oi","Olá","Como vai?"};
+  
+  lista[0]="Uma String qualquer";
+  
+  for (int i = 0; i < lista.length; i++) {
+   String string = lista[i];
+   
+  }
+  
 
-		//Declaração de uma lista dinâmica do tipo ArrayList
-		ArrayList<String> listaDinamica = new ArrayList<String>();
-		
-		listaDinamica.add("Primeiro");
-		listaDinamica.add("Segundo");
-		listaDinamica.add(1, "Um e meio");
-		
-		for(String i : listaDinamica) {
-			System.out.print("\n" + i);
-		}
-		
-		//HashMap (também conhecido como Dicionário)
-		
-		HashMap<String, String> arvore = new HashMap<String, String>();
-
-	}
+  ArrayList<String> listaDinamica=new ArrayList<String>();
+  
+  listaDinamica.add("Oi");
+  listaDinamica.add("Como vai?");
+  listaDinamica.set(1, "Alo");
+  listaDinamica.isEmpty();
+  listaDinamica.add(0, "Mais um");
+  
+  
+  for (int i = 0; i < listaDinamica.size(); i++) {
+   String item = listaDinamica.get(i);
+   System.out.println(item);
+  }
+  
+  /*for (String item : listaDinamica) {
+   System.out.println(item);
+  }*/
+  
+  HashMap<String, String> arvore=new HashMap<>();
+  arvore.put("01A", "A casa e azul");
+  arvore.put("01B", "A casa e rosa");
+  //arvore.put("01B", "A casa e preta");
+  
+  String valor=arvore.get("01A");
+  
+  for (String item : arvore.keySet()) {
+   System.out.println(item);
+  }
+  
+ }
 
 }
